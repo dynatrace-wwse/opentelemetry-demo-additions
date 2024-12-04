a472b906ecfb6827af4a85a860894094ee669e22
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2024-12-04 10:47:54 +0000

    otel gateway and enrichment
    Signed-off-by: Sergio Hinojosa <sergio.hinojosa@dynatrace.com>

--- alpha-cluster/install-astroshop.sh
+++ alpha-cluster/install-astroshop.sh
@@ -5,8 +5,52 @@
 # git clone ssh://git@bitbucket.lab.dynatrace.org/cloude/opentelemetry-demo-additions.git 
 # cd opentelemetry-demo-additions/opentelemetry-demo-k8s
 
+## 1 - INSTALL OPERATOR
+helm upgrade --install dynatrace-operator oci://public.ecr.aws/dynatrace/dynatrace-operator \
+--create-namespace \
+--namespace dynatrace \
+--atomic \
+--version 1.3.2
+
+kubectl -n dynatrace create secret generic [DYNAKUBE_NAME] --from-literal="apiToken=<DT_API_TOKEN>" --from-literal="dataIngestToken=<DT_INGEST_TOKEN>"
+
+kubectl apply -f dynakube-oteldemo.yaml --namespace dynatrace
+
+
+# INSTALL OPENTEL-OPERATOR
+
+#Generate Dynatrace Access Tokens
+#Dynatrace API token (DT_API_TOKEN)
+#openTelemetryTrace.ingest
+#metrics.ingest
+#logs.ingest
+
+## 2 - INOSTALL Cert-Manager
+helm repo add jetstack https://charts.jetstack.io --force-update
+
+helm install cert-manager jetstack/cert-manager --namespace cert-manager \
+--create-namespace \
+--version v1.15.3 \
+--set crds.enabled=true
+
 helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
 helm repo update
+
+helm install opentelemetry-operator open-telemetry/opentelemetry-operator --set "manager.collectorImage.repository=hcr.io/dynatrace/dynatrace-otel-collector/dynatrace-otel-collector:0.14.0" --create-namespace --namespace opentelemetry-operator-system --version 0.67.0
+
+kubectl create secret generic dynatrace-otelcol-dt-api-credentials --from-literal=DT_ENDPOINT=https://sro97894.live.dynatrace.com/api/v2/otlp --from-literal=DT_API_TOKEN=dt0c01.VH7PNJNMB6K2Q3MTLLYCRGQ7.xxx --namespace staging-astroshop
+# do for production-astroshop, staging-astroshop,
+kubectl apply -f otel_collector_gateway.yaml --namespace astroshop
+# do for production-astroshop, staging-astroshop, 
+
+kubectl apply -f otel_k8s_enrichment.yaml
+kubectl apply -f otel_k8s_enrichment-staging.yaml
+kubectl apply -f otel_k8s_enrichment-production.yaml
+
+# Install OtelDemo!!
+
+# GET Kubectl  YAML files
+
 
 #helm upgrade --install -f ./install/bring-your-own-observability-helm-values.yaml \
 #astroshop \
@@ -70,19 +114,13 @@ kubectl label namespace astroshop dynatrace.com/inject=true
 # Patch the astroshop deployments
 
 bash patch.sh
-
 #kubectl delete deployment astroshop-frontendproxy --namespace astroshop
 #kubectl delete service astroshop-frontendproxy --namespace astroshop
 
-
-# Install openteöemetry 
-
+# Install opentelemetry 
 helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
 helm repo update
-
 helm install opentelemetry-operator open-telemetry/opentelemetry-operator --set "manager.collectorImage.repository=hcr.io/dynatrace/dynatrace-otel-collector/dynatrace-otel-collector:0.14.0" --create-namespace --namespace opentelemetry-operator-system --version 0.67.0
-
 kubectl create secret generic dynatrace-otelcol-dt-api-credentials --from-literal=DT_ENDPOINT=https://sro97894.live.dynatrace.com/api/v2/otlp --from-literal=DT_API_TOKEN=dt0c01.4IM3DHAT2JIT5OFNLEHHDIML.xxx --namespace astroshop
-
 kubectl apply -f otel_collector_gateway.yaml --namespace astroshop
 kubectl apply -f otel_k8s_enrichment.yaml
